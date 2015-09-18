1593e7002318ee1eb9e7a0ffec30ffb553d4113d
sarrionandia <r@sarrionandia.com>
2015-09-18 09:54:38 +0100

    Show list of books

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,13 +1,34 @@
 package com.twu.biblioteca;
 
+import java.time.Year;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class BibliotecaApp {
 
     private static final String WELCOME = "Welcome to Biblioteca!";
 
+    static List<Book> bookList = new ArrayList<Book>();
+
+    private static void setupBookList(){
+        bookList.add(new Book("Pride and Prejudice", "Jane Austen", Year.of(1813)));
+        bookList.add(new Book("To Kill a Mockingbird", "Harper Lee", Year.of(1960)));
+        bookList.add(new Book("The Great Gatsby", "F. Scott Fitzgerald", Year.of(1925)));
+        bookList.add(new Book("Frankenstein", "Mary Shelley", Year.of(1818)));
+    }
+
     public static void main(String[] args) {
+
+        setupBookList();
 
         // Display a welcome message
         System.out.println(WELCOME);
+
+        //Print the list of books
+        for (Book book : bookList) {
+            System.out.println(book);
+        }
 
 
     }
